feeea572dfa722b82a03f160da7047f25e8b1412
liyijie <liyijie825@gmail.com>
2015-08-05 23:33:06 +0800

    modify user email index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805151017) do
+ActiveRecord::Schema.define(version: 20150805153232) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20150805151017) do
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token"
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email"
   add_index "users", ["phone"], name: "index_users_on_phone", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
