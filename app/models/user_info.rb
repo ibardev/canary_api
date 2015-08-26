47a5bd797d24e44f028ac012cf37e267c301d12b
liyijie <liyijie825@gmail.com>
2015-08-26 17:52:01 +0800

    update annotation

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -2,19 +2,23 @@
 #
 # Table name: user_infos
 #
-#  id            :integer          not null, primary key
-#  sex           :integer
-#  nickname      :string
-#  birth         :date
-#  dest_province :string
-#  dest_city     :string
-#  province      :string
-#  city          :string
-#  contact_type  :integer
-#  contact       :string
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
-#  user_id       :integer
+#  id                  :integer          not null, primary key
+#  sex                 :integer
+#  nickname            :string
+#  birth               :date
+#  dest_province       :string
+#  dest_city           :string
+#  province            :string
+#  city                :string
+#  contact_type        :integer
+#  contact             :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  user_id             :integer
+#  avatar_file_name    :string
+#  avatar_content_type :string
+#  avatar_file_size    :integer
+#  avatar_updated_at   :datetime
 #
 # Indexes
 #
