c6d0f115721f9b0cb0a802dcc7275fce314286cc
liyijie <liyijie825@gmail.com>
2015-10-24 10:53:51 +0800

    add home index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'home/index'
+  
+  root 'home#index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   ######################################################
