22bc62b7642272f4461f0fd4aab261672fdd124d
liyijie <liyijie825@gmail.com>
2015-11-10 18:07:08 +0800

    discover bug fix

--- app/models/discover.rb
+++ app/models/discover.rb
@@ -21,13 +21,8 @@ class Discover < ActiveRecord::Base
   belongs_to :discoverable, polymorphic: true
   belongs_to :user
 
-  before_save :default_values
-
   scope :unblock, -> { where.not(block: true) }
   
   default_scope { order('updated_at DESC') }
 
-  def default_values
-    self.block ||= false
-  end
 end
