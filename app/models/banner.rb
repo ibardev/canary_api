bc28af46e9d7261ce95a3cecf236fc5cd5e18655
liyijie <liyijie825@gmail.com>
2015-12-05 17:31:46 +0800

    add cover_image to banner model

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -10,5 +10,6 @@
 #
 
 class Banner < ActiveRecord::Base
-  
+  has_one :cover_image, -> { where photo_type: "cover" }, class_name: "Image", as: :imageable, dependent: :destroy
+  accepts_nested_attributes_for :cover_image, allow_destroy: true
 end
