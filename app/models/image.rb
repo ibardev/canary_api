cad903f06b3e6f5cd4d8e43cbc71924510ee6ea3
liyijie <liyijie825@gmail.com>
2016-02-24 17:47:30 +0800

    adjust image size

--- app/models/image.rb
+++ app/models/image.rb
@@ -23,7 +23,7 @@ class Image < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
 
   # paperclip gem
-  has_attached_file :photo, styles: { mini: '48x48>', small: '100x100>', medium: '200x200>', product: '320x320>', large: '600x600>' }
+  has_attached_file :photo, styles: { mini: '48x48>', small: '150x150>', medium: '300x300>', product: '600x600>', large: '1024x1024>' }
 
   validates_attachment_presence :photo
   validates_attachment_size     :photo, less_than: 5.megabytes
