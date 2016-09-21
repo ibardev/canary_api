4a83c4f0ac8e770d1d247dc171630bf2578b4a91
liyijie <liyijie825@gmail.com>
2016-09-21 13:11:19 +0800

    add home controller fetch share api

--- Gemfile
+++ Gemfile
@@ -24,6 +24,9 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
+# Simple, but flexible HTTP client library, with support for multiple backends.
+# Read more: https://github.com/lostisland/faraday
+gem 'faraday'
 
 ##########################################################################################
 # Authentication
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,18 @@
 class HomeController < ApplicationController
-  
+
   wechat_api
 
   def index
+
+    connection = Faraday::Connection.new "http://wechat-api.tallty.com"
+    response = connection.get '/lvke_wechat/js_hash.json', { page_url: "http://www.lvkeapp.com" }
+    logger.info "response is:#{MultiJson.load response.body}"
+
+    @js_hash = MultiJson.load response.body
+
+
     user_agent = request.env['HTTP_USER_AGENT'].downcase 
+
     # if user_agent.include?("iphone") || user_agent.include?("android")
     @is_safari = user_agent.include? "safari"
   end
