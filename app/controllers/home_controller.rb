0c9c92a3dbad390453c08fe383bd4771a8b8a586
liyijie <liyijie825@gmail.com>
2016-09-21 12:13:03 +0800

    add wechat_api to HomeController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  
+  wechat_api
+
   def index
     user_agent = request.env['HTTP_USER_AGENT'].downcase 
     # if user_agent.include?("iphone") || user_agent.include?("android")
