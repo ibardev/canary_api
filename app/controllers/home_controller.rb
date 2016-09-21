620ccdcc715932000fec6cc38a7d41b1b1e0f1a8
liyijie <liyijie825@gmail.com>
2016-09-21 12:11:29 +0800

    add home/index page wechat share

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     user_agent = request.env['HTTP_USER_AGENT'].downcase 
+    # if user_agent.include?("iphone") || user_agent.include?("android")
     @is_safari = user_agent.include? "safari"
   end
 end
